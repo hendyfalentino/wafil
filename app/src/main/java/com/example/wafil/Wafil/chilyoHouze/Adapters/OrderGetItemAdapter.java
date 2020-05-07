c154b3d81d719750e9417041e7d35567afa406ad
Chrisdityra Lengkey <61264981+chlengkey@users.noreply.github.com>
2020-05-07 13:56:09 +0800

    fix

---
+++ app/src/main/java/com/example/wafil/Wafil/chilyoHouze/Adapters/OrderGetItemAdapter.java
@@ -0,0 +1,57 @@
+package com.example.wafil.Wafil.chilyoHouze.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.wafil.R;
+import com.example.wafil.Wafil.chilyoHouze.Model.PaymentItem;
+import com.example.wafil.Wafil.chilyoHouze.Support.Support;
+
+import java.util.List;
+
+public class OrderGetItemAdapter extends RecyclerView.Adapter<OrderGetItemAdapter.ViewHolder> {
+
+    private List<PaymentItem> dataList;
+    private Context context;
+
+    public OrderGetItemAdapter(Context context, List<PaymentItem> dataList){
+        this.context = context;
+        this.dataList = dataList;
+    }
+
+    @NonNull
+    @Override
+    public OrderGetItemAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.payment_item, parent, false);
+        return new OrderGetItemAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull OrderGetItemAdapter.ViewHolder holder, int position) {
+        final PaymentItem v = dataList.get(position);
+        holder.order_qty.setText(v.getProduct_qty()+" x");
+        holder.order_name.setText(v.getProduct_name());
+        holder.order_price.setText(Support.rupiahFormat(v.getProduct_price())+" coin");
+    }
+
+    @Override
+    public int getItemCount() {return dataList.size();}
+
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        TextView order_qty, order_name, order_price;
+
+        ViewHolder(@NonNull View itemView){
+            super(itemView);
+            order_qty = itemView.findViewById(R.id.order_qty);
+            order_name = itemView.findViewById(R.id.order_name);
+            order_price = itemView.findViewById(R.id.order_price);
+        }
+    }
+}
--- app/src/main/java/com/example/wafil/Wafil/chilyoHouze/activity_chilyo_order.java
+++ app/src/main/java/com/example/wafil/Wafil/chilyoHouze/activity_chilyo_order.java
@@ -1,6 +1,4 @@
 package com.example.wafil.Wafil.chilyoHouze;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class activity_chilyo_order extends AppCompatActivity {
+public class activity_chilyo_order {
 }

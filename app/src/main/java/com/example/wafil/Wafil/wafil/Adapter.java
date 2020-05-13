b3fdb9944c12888ec02a1740b37e03c4cd3bef8a
hendyfalentino <61306546+hendyfalentino@users.noreply.github.com>
2020-05-14 01:45:59 +0800

    Try to add Shopping Cart from Wafil

--- app/src/main/java/com/example/wafil/Wafil/wafil/Adapter.java
+++ app/src/main/java/com/example/wafil/Wafil/wafil/Adapter.java
@@ -1,15 +1,19 @@
 package com.example.wafil.Wafil.wafil;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.wafil.R;
+import com.example.wafil.Wafil.chilyoHouze.MainServiceAndProduct.ProductDetail;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
@@ -32,13 +36,23 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder>{
     }
 
     @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
         holder.name_product.setText(product.get(position).getName_product());
         holder.price.setText(product.get(position).getPrice());
 
         String test = product.get(position).getPic_user();
         String img = "http://carexports.uk/Admin/assets/img/" + test + ".png";
         Picasso.with(context).load(img).into(holder.imageView);
+
+        // ke product detail
+        holder.shopping_item_container.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mIntent = new Intent(view.getContext(), ProductDetail.class);
+                mIntent.putExtra("product_id", product.get(position).getProduct_id());
+                view.getContext().startActivity(mIntent);
+            }
+        });
     }
 
     @Override
@@ -50,12 +64,13 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder>{
 
         TextView name_product, price;
         ImageView imageView;
-
+        ConstraintLayout shopping_item_container;
         public MyViewHolder(View itemView){
             super(itemView);
             name_product = itemView.findViewById(R.id.name_product);
             price = itemView.findViewById(R.id.price);
             imageView = itemView.findViewById(R.id.shopping_item_image);
+            shopping_item_container = itemView.findViewById(R.id.shopping_item_container);
         }
     }
 }
--- app/src/main/java/com/example/wafil/Wafil/wafil/Product.java
+++ app/src/main/java/com/example/wafil/Wafil/wafil/Product.java
@@ -4,9 +4,14 @@ import com.google.gson.annotations.SerializedName;
 
 public class Product {
 
+    @SerializedName("product_id") private String product_id;
     @SerializedName("name_product") private String name_product;
     @SerializedName("price") private String price;
     @SerializedName("pic_user") private String pic_user;
+
+    public String getProduct_id() {
+        return product_id;
+    }
 
     public String getName_product() {
         return name_product;
